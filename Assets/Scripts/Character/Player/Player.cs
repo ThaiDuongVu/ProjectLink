150e2586b00148b5b14902e2767093cfa94b8e68
Duong Vu <vu.t.duong01@gmail.com>
2023-07-07 17:19:00 +0700

    Update player UI references

--- Assets/Scripts/Character/Player/Player.cs
+++ Assets/Scripts/Character/Player/Player.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Player : Character
 {
-    
+    [Header("UI References")]
+
+    [SerializeField] private Image healthBar;
+    public override float Health
+    {
+        get => base.Health;
+        set
+        {
+            base.Health = value;
+            healthBar.fillAmount = value / baseHealth;
+        }
+    }
+
+    [SerializeField] private TMP_Text coinText;
+    private int _collectedCoins;
+    public int CollectedCoins
+    {
+        get => _collectedCoins;
+        set
+        {
+            _collectedCoins = value;
+            coinText.SetText(value.ToString());
+        }
+    }
 }
