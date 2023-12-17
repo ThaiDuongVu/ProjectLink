c35d758255d5bacdc65440ea4e12c95be0ee2f0f
Duong Vu <vu.t.duong01@gmail.com>
2023-12-17 15:17:58 -0500

    Add moving platform

---
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    #region Unity Events
+
+    protected virtual void Awake()
+    {
+
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        
+    }
+
+    protected virtual void FixedUpdate()
+    {
+
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Interactable/MovingPlatform.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class MovingPlatform : Interactable
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2[] positions = new Vector2[2];
+
+    private int _fromIndex;
+    private int _toIndex;
+    private Vector2 FromPosition => positions[_fromIndex];
+    private Vector2 ToPosition => positions[_toIndex];
+
+    private Rigidbody2D _rigidbody;
+    private BoxCollider2D _boxCollider;
+    private SpriteRenderer _sprite;
+
+    #region Unity Events
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        _fromIndex = 0;
+        _toIndex = 1;
+
+        _boxCollider.size = _sprite.size;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Vector2.Distance(transform.localPosition, ToPosition) <= 0.1f)
+        {
+            if (_fromIndex < positions.Length - 1) _fromIndex++;
+            else _fromIndex = 0;
+            if (_toIndex < positions.Length - 1) _toIndex++;
+            else _toIndex = 0;
+        }
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        _rigidbody.MovePosition(_rigidbody.position + speed * Time.fixedDeltaTime * (ToPosition - FromPosition).normalized);
+    }
+
+    #endregion
+}
