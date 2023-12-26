f3e643caadba0de514deeef76c80a5c08fdff99e
Duong Vu <vu.t.duong01@gmail.com>
2023-12-25 19:32:43 -0500

    Update level 21

--- Assets/Scripts/Interactable/Switch.cs
+++ Assets/Scripts/Interactable/Switch.cs
@@ -85,7 +85,7 @@ public class Switch : Interactable
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Block") || other.GetComponent<Brick>())
+        if (other.CompareTag("Block") || other.GetComponent<Brick>() || other.GetComponent<Scale>())
         {
             Toggle();
         }
